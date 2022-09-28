1d6b42f592820c7191e47b4186bd517b42ef0578
Nursultanuly <dnursultanuly@gmail.com>
2022-09-28 16:14:51 +0600

    third

---
+++ lab3.py
@@ -0,0 +1,23 @@
+'''
+for i in range(0,51,2):
+    print(i)
+'''
+'''
+i = 0
+while i < 64:
+    print(i)
+    i = i + 8
+'''
+'''
+number = 0
+while number < 5:
+    number += 1
+    if number == 3:
+        continue
+    print(f"number = {number}")
+'''
+'''
+for T1 in  "ao":
+    for T2 in "bc":
+        print(f"{T1}{T2}")
+'''
