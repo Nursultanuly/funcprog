b8949def95068782bdc3eeee78124520b8fbde26
Nursultanuly <dnursultanuly@gmail.com>
2022-10-05 16:21:46 +0600

    4

--- lab3.py
+++ lab3.py
@@ -13,11 +13,11 @@ number = 0
 while number < 5:
     number += 1
     if number == 3:
-        continue
+        continue #break
     print(f"number = {number}")
 '''
 '''
-for T1 in  "ao":
+for T1 in "ao":
     for T2 in "bc":
         print(f"{T1}{T2}")
 '''
---
+++ lab4.py
@@ -0,0 +1,27 @@
+'''
+s = input()
+q = s.find('t')
+print(q)
+print(len(s))
+print(s[1:5])
+n = input()
+print(s+n)
+'''
+
+'''
+a = "12345645124"
+line = a.replace('12', ':')
+print(line)
+'''
+'''
+b = "Atalanta"
+#k = b.lower()
+#m = b.title()
+t = "a"
+count = b.count(t)
+print(count)
+'''
+'''
+c = "almaty"
+print(c.capitalize())
+'''
